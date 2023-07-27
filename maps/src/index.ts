c0389e757e862250e377a1b76da2efcb81ee750e
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-07-26 23:14:14 -0400

    update map script

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,17 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
+// const user = new User();
 
-console.log(user);
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
 
+new google.maps.Map(document.getElementById('map'),{
+  zoom:1,
+  center:{
+    lat: 0,
+    lng: 0
+  }
+}) as HTMLElement;
